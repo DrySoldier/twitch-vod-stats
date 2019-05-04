31ff6fd9e260c4ffbfaaeaf9d961f6fa2ac174a2
christiancotham61 <christiancotham61@gmail.com>
2019-05-04 09:13:40 -0400

    removed some unneeded dependecies

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
 let localHost = 'http://localhost:3000/';
-let api = 'https://ngo9nlp0nf.execute-api.us-east-1.amazonaws.com/dev/'
+let api = 'https://ngo9nlp0nf.execute-api.us-east-1.amazonaws.com/dev'
 
 export default {
   createStats: function (id) {
     console.log('create stats');
 
-    return axios.get(`${api}${id}`);
+    return axios.get(`${api}/${id}`);
   },
   getStats: function (id) {
     console.log('get stats')
-    return axios.get(`${api}vods/${id}`)
+    return axios.get(`${api}/vods/${id}`)
   },
   areStatsCreated: function(id){
     console.log('are stats created');
-    return axios.get(`${api}statTest/${id}`)
+    return axios.get(`${api}/statTest/${id}`)
   }
 };
